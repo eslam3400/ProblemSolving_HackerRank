ffe0181445d825c75d1adb20f8ac5aa18fd9e2c7
Eslam Magdy <eslam3400@gmail.com>
2021-08-14 16:12:34 +0200

    one more ♪

--- src/Devtik/SubarrayDivision.java
+++ src/Devtik/SubarrayDivision.java
@@ -3,7 +3,6 @@ package Devtik;
 import java.util.Scanner;
 
 public class SubarrayDivision {
-<<<<<<< HEAD
     public static void main(String[] args) {
         Scanner get = new Scanner(System.in);
         System.out.println("Please enter the squares number of chocolate bar");
@@ -37,33 +36,3 @@ public class SubarrayDivision {
         return divideWays;
     }
 }
-=======
-
-    static  Scanner get = new Scanner(System.in);
-
-    public static void main(String args[]){
-        System.out.println("Please enter the number of segments");
-        int segments[] = new int [get.nextInt()];
-        System.out.println("Please enter ron's birth day");
-        int day = get.nextInt();
-        System.out.println("Please enter ron's birth month");
-        int month = get.nextInt();
-        fillSegments(segments);
-    }
-
-    protected static void fillSegments(int array[]){
-        int n = -1;
-        for (int i = 0; i < array.length; i++) {
-            n = get.nextInt();
-            if (1 <= n && n <= 5) array[i] = n;
-        }
-    }
-
-    protected static int process(int array[],int numberOfSquares,int sumOfSquares){
-        for (int i = 0; i < array.length; i++) {
-
-        }
-        return 0;
-    }
-}
->>>>>>> 1e3b4930a83ca55f9e462c5eca046b580588b3a7
